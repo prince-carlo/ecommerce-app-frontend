cff119b8fdc6d2aa7557301e8f4eedbb1faeffe7
Prince Carlo Calapre <nprincecarl@gmail.com>
2026-05-31 11:25:58 +0800

    [MODIFY] - Adjusted the layout design.

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
-import {Navbar, Footer} from "@/components";
+import { Navbar, Footer } from "@/components";
+import { Box } from "@chakra-ui/react";
 
 
 export default function MainLayout() {
   return (
-    <>
+    <Box minH={"100vh"} display={"flex"} flexDirection={"column"}>
       <Navbar />
-      <main>
+      <Box as="main" flex={"1"}>
         <Outlet />
-      </main>
+      </Box>
       <Footer />
-    </>
+    </Box>
   );
 }
